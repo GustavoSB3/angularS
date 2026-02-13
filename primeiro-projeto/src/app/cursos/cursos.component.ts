b6803e0f358742b83019f54a5317511e84cb5613
Gustavo Berg <gustavo2berg@gmail.com>
2026-02-13 16:12:44 -0300

    injector

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -15,6 +15,8 @@ export class CursosComponent implements OnInit {
     this.nomePortal = "http://loiane.training";
 
     this.cursos = this.cursosService.getCursos();
+
+    var servico = new CursosService();
   }
 
   ngOnInit() {}
